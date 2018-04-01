9c27f98b88eca624a51673b64f955521f873ce3b
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-03-31 22:44:51 -0400

    Parsed discovery and contained date of fires.

--- wildfires.py
+++ wildfires.py
@@ -5,7 +5,10 @@ import pandas as pd
 
 con = sqlite3.connect("RDS-2013-0009.4_SQLite/Data/FPA_FOD_20170508.sqlite")
 cur = con.cursor()
-table_names = cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
-print(table_names.fetchall())
-wildfires = pd.read_sql("select * from fires limit 5", con)
-print(wildfires)
+fire_durations = pd.read_sql("select DISCOVERY_DATE,DISCOVERY_DOY,DISCOVERY_TIME,CONT_DATE,CONT_DOY,CONT_TIME from fires limit 20", con)
+epoch = pd.to_datetime(0, unit="s").to_julian_date() # start of epoch on the Julian Calendar
+
+# discovery date and contained date is in Julian days, converts to dates.
+fire_durations.DISCOVERY_DATE = pd.to_datetime(fire_durations.DISCOVERY_DATE - epoch, unit="D")
+fire_durations.CONT_DATE = pd.to_datetime(fire_durations.CONT_DATE - epoch, unit="D")
+print(fire_durations)
