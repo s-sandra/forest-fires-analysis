9ee69d8e386af3dd563c8ee70041f322d522b1b2
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-04-02 16:43:24 -0400

    Obtained land unit type for fire.

--- wildfires.py
+++ wildfires.py
@@ -9,7 +9,7 @@ cur = con.cursor()
 # creates dataframe where fire durations are expressed in total minutes.
 fire_durations = pd.read_sql("select cont_date * 24 * 60 + substr(cont_time, 1, 2) * 60 + "
                              "substr(cont_time, 3, 2) - discovery_date * 24 * 60 + substr(discovery_time, 1, 2) * 60 + "
-                             "substr(discovery_time, 3, 2) from fires", con)
+                             "substr(discovery_time, 3, 2) duration from fires", con)
 
 # epoch = pd.to_datetime(0, unit="s").to_julian_date() # start of epoch on the Julian Calendar
 
@@ -17,5 +17,8 @@ fire_durations = pd.read_sql("select cont_date * 24 * 60 + substr(cont_time, 1, 
 # fire_durations.DISCOVERY_DATE = pd.to_datetime(fire_durations.DISCOVERY_DATE - epoch, unit="D")
 # fire_durations.CONT_DATE = pd.to_datetime(fire_durations.CONT_DATE - epoch, unit="D")
 
+# what if time is none, but date appears?
+# some fire acres are placed in classes. Should we ignore those
 fire_durations = fire_durations.dropna()
-print(fire_durations)
+land_owner = pd.read_sql("select UnitType from NWCG_UnitIDActive_20170109 limit 20", con)
+print(land_owner)
