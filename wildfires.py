91b056156e2cbc402f74478b9a298b5312b10e26
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-04-02 16:05:44 -0400

    Calculated fire duration (as minutes) using discovery and contained date and time.

--- wildfires.py
+++ wildfires.py
@@ -5,10 +5,17 @@ import pandas as pd
 
 con = sqlite3.connect("RDS-2013-0009.4_SQLite/Data/FPA_FOD_20170508.sqlite")
 cur = con.cursor()
-fire_durations = pd.read_sql("select DISCOVERY_DATE,DISCOVERY_DOY,DISCOVERY_TIME,CONT_DATE,CONT_DOY,CONT_TIME from fires limit 20", con)
-epoch = pd.to_datetime(0, unit="s").to_julian_date() # start of epoch on the Julian Calendar
 
-# discovery date and contained date is in Julian days, converts to dates.
-fire_durations.DISCOVERY_DATE = pd.to_datetime(fire_durations.DISCOVERY_DATE - epoch, unit="D")
-fire_durations.CONT_DATE = pd.to_datetime(fire_durations.CONT_DATE - epoch, unit="D")
+# creates dataframe where fire durations are expressed in total minutes.
+fire_durations = pd.read_sql("select cont_date * 24 * 60 + substr(cont_time, 1, 2) * 60 + "
+                             "substr(cont_time, 3, 2) - discovery_date * 24 * 60 + substr(discovery_time, 1, 2) * 60 + "
+                             "substr(discovery_time, 3, 2) from fires", con)
+
+# epoch = pd.to_datetime(0, unit="s").to_julian_date() # start of epoch on the Julian Calendar
+
+# discovery date and contained date is in Julian days (as float), converts to timestamp.
+# fire_durations.DISCOVERY_DATE = pd.to_datetime(fire_durations.DISCOVERY_DATE - epoch, unit="D")
+# fire_durations.CONT_DATE = pd.to_datetime(fire_durations.CONT_DATE - epoch, unit="D")
+
+fire_durations = fire_durations.dropna()
 print(fire_durations)
