9dc664a59e345e156d3ff99b77569c83fdb828b8
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-04-03 13:22:38 -0400

    Joined fires' nwcg unit id active column on the UnitType column from nwcg_unitidactive_20170109 to obtain (potentially) the landowner of the area where the fire occurred.

--- wildfires.py
+++ wildfires.py
@@ -9,11 +9,12 @@ cur = con.cursor()
 # creates dataframe where fire durations are expressed in total minutes.
 fire_durations = pd.read_sql("select fod_id, cont_date * 24 * 60 + substr(cont_time, 1, 2) * 60 + "
                              "substr(cont_time, 3, 2) - discovery_date * 24 * 60 + substr(discovery_time, 1, 2) * 60 + "
-                             "substr(discovery_time, 3, 2) FIRE_DURATION from fires", con)
+                             "substr(discovery_time, 3, 2) fire_duration from fires", con)
 
 # what if time is none, but date appears?
-# some fire acres are placed in classes. Should we ignore those
+# some fire acres are placed in classes. Should we ignore those?
 
 # extracts primary key FOD_ID and the unit type (landowner) from the agency that reported the fire.
+# value in UnitType column can be US Federal, US State, US County/Local, Tribe or Interagency.
 land_owner = pd.read_sql("select fod_id, UnitType  from fires join nwcg_unitidactive_20170109 "
                         "on nwcg_reporting_unit_id==unitid", con)
