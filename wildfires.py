29e7ea9a0cb8dbd2feb205f9aa1e7d8518f560d2
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-04-03 13:17:19 -0400

    Joined fires' nwcg unit id active column on the UnitType column from nwcg_unitidactive_20170109 to obtain (potentially) the landowner of the area where the fire occurred.

--- wildfires.py
+++ wildfires.py
@@ -7,18 +7,13 @@ con = sqlite3.connect("RDS-2013-0009.4_SQLite/Data/FPA_FOD_20170508.sqlite")
 cur = con.cursor()
 
 # creates dataframe where fire durations are expressed in total minutes.
-fire_durations = pd.read_sql("select cont_date * 24 * 60 + substr(cont_time, 1, 2) * 60 + "
+fire_durations = pd.read_sql("select fod_id, cont_date * 24 * 60 + substr(cont_time, 1, 2) * 60 + "
                              "substr(cont_time, 3, 2) - discovery_date * 24 * 60 + substr(discovery_time, 1, 2) * 60 + "
-                             "substr(discovery_time, 3, 2) duration from fires", con)
-
-# epoch = pd.to_datetime(0, unit="s").to_julian_date() # start of epoch on the Julian Calendar
-
-# discovery date and contained date is in Julian days (as float), converts to timestamp.
-# fire_durations.DISCOVERY_DATE = pd.to_datetime(fire_durations.DISCOVERY_DATE - epoch, unit="D")
-# fire_durations.CONT_DATE = pd.to_datetime(fire_durations.CONT_DATE - epoch, unit="D")
+                             "substr(discovery_time, 3, 2) FIRE_DURATION from fires", con)
 
 # what if time is none, but date appears?
 # some fire acres are placed in classes. Should we ignore those
-fire_durations = fire_durations.dropna()
-land_owner = pd.read_sql("select UnitType from NWCG_UnitIDActive_20170109 limit 20", con)
-print(land_owner)
+
+# extracts primary key FOD_ID and the unit type (landowner) from the agency that reported the fire.
+land_owner = pd.read_sql("select fod_id, UnitType  from fires join nwcg_unitidactive_20170109 "
+                        "on nwcg_reporting_unit_id==unitid", con)
